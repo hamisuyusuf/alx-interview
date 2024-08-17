58c5228b23dc94b9e0038a6b499d38bd87e26273
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-17 08:04:00 +0000

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,29 @@
+#!/usr/bin/python3
+"""
+Module 0-lockboxes
+This module contains the canUnlockAll function, which determines
+if all the boxes in a list can be opened.
+"""
+
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+    
+    Parameters:
+        boxes (list of lists): A list where each element is a list of keys
+                               available in that box.
+    
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)
-    opened = set([0])  # Start with the first box unlocked
-    keys = set(boxes[0])  # Get keys from the first box
+    opened = set([0])  # Set to keep track of opened boxes, starting with box 0
+    keys = set(boxes[0])  # Collect keys from the first box (which is already open)
     
     while keys:
-        key = keys.pop()  # Take one key from the set
+        key = keys.pop()  # Remove a key from the set
         if key < n and key not in opened:
             opened.add(key)  # Mark the box corresponding to the key as opened
             keys.update(boxes[key])  # Add new keys found in the newly opened box
     
-    return len(opened) == n  # Check if all boxes are opened
+    return len(opened) == n  # Return True if all boxes are opened, else False
