1032146f5edc7e83df72ff54e8f70e89ddae6a2d
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-09-06 22:26:39 +0000

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-UTF-8 encoding is variable-width
+UTF-8 Validation
+Determines if a given data set represents a valid UTF-8 encoding.
 """
 
 
@@ -9,29 +10,24 @@ def validUTF8(data):
     Determines if a given data set represents a valid UTF-8 encoding.
     return: True if valid UTF-8 encoding, False otherwise
     """
+    num_bytes = 0  # Number of bytes to follow in the current UTF-8 character
 
-    num_bytes = 0
-
-    # Masks for checking the leading bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
+    mask1 = 1 << 7
+    mask2 = 1 << 6
 
     for byte in data:
-        # Check only the 8 least significant bits of each integer
         byte = byte & 0xFF
 
         if num_bytes == 0:
-            # Count the number of leading
-            if (byte >> 5) == 0b110:  # 2-byte character
+            if (byte >> 5) == 0b110:
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:  # 3-byte character
+            elif (byte >> 4) == 0b1110:
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:  # 4-byte character
+            elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            elif (byte >> 7) != 0:  # 1-byte character (0xxxxxxx)
+            elif (byte >> 7) != 0:
                 return False
         else:
-            # Check that the byte starts with 10xxxxxx
             if (byte >> 6) != 0b10:
                 return False
             num_bytes -= 1
